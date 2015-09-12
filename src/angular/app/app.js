5596d6b285e729b7ba115f99f5f5f06a2ae72773
Chris Tanner <chris.tanner@sslgb.com>
2015-09-12 17:36:45 +0100

    added basic views to angular

--- src/angular/app/app.js
+++ src/angular/app/app.js
@@ -2,11 +2,25 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'ui.router',
   'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home',{
+        url: '/',
+        views: {
+            'header': {
+                templateUrl: 'templates/partials/header.html'
+            },
+            'sidebar': {
+                templateUrl: 'templates/partials/sidebar.html'
+            },
+            'content': {
+                templateUrl: 'templates/partials/content.html'
+            }
+        }
+    })
+});
