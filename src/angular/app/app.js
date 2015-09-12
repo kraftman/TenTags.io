8e2009f22f18f3803f67d86c65a64c0da0e3a775
Chris Tanner <chris.tanner@sslgb.com>
2015-09-12 21:49:59 +0100

    got basic views to switch correctly

--- src/angular/app/app.js
+++ src/angular/app/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ui.router',
   'myApp.version'
-]).
-config(function($stateProvider, $urlRouterProvider) {
+]);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
@@ -15,12 +15,39 @@ config(function($stateProvider, $urlRouterProvider) {
             'header': {
                 templateUrl: 'templates/partials/header.html'
             },
-            'sidebar': {
-                templateUrl: 'templates/partials/sidebar.html'
+            'filterbar': {
+                templateUrl: 'templates/partials/filterbar.html',
+                controller: 'FilterBarController'
             },
             'content': {
                 templateUrl: 'templates/partials/content.html'
             }
         }
     })
+    .state('home.filters',{
+        url: 'f/:subname',
+        views: {
+          'content@': {
+              templateUrl: 'templates/partials/content2.html',
+              controller: 'FilterController'
+          }
+        }
+    })
 });
+
+myApp.controller('FilterController', ['$scope','$stateParams', function($scope,$stateParams) {
+  $scope.subname = $stateParams.subname;
+
+}]);
+
+myApp.controller('FilterBarController',function($scope,$http) {
+  $http.get('/static/defaultsubs.json').success(function(data) {
+    $scope.subs = data;
+  });
+});
+
+myApp.controller('ContentController',function($scope,$http) {
+  $http.get('/static/defaultsubs.json').success(function(data) {
+    $scope.subs = data;
+  });
+})
