706dfb57c2411069c579b1690c1876523afe0772
Chris Tanner <me@itschr.is>
2015-11-17 23:50:18 +0000

    test

--- apisetup/apisetup.sh
+++ apisetup/apisetup.sh
@@ -2,7 +2,7 @@ apt-get install -y lua5.1 libpcre3 libpcre3-dev libuuid-dev
 
 wget https://openresty.org/download/ngx_openresty-1.9.3.1.tar.gz
 tar xzvf ngx_openresty-1.9.3.1.tar.gz
-cd ngx_openresty-VERSION/
+cd ngx_openresty-1.9.3.1/
 ./configure
 make
 make install
