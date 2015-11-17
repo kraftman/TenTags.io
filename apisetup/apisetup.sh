ccdf679d359d94e2aacc8c6d11ff62f862f18776
Chris Tanner <me@itschr.is>
2015-11-17 23:48:49 +0000

    add mising files

---
+++ apisetup/apisetup.sh
@@ -0,0 +1,13 @@
+apt-get install -y lua5.1 libpcre3 libpcre3-dev libuuid-dev
+
+wget https://openresty.org/download/ngx_openresty-1.9.3.1.tar.gz
+tar xzvf ngx_openresty-1.9.3.1.tar.gz
+cd ngx_openresty-VERSION/
+./configure
+make
+make install
+
+
+luarocks install luasec OPENSSL_LIBDIR=/usr/lib/x86_64-linux-gnu/
+luarocks install lapis
+luarocks install magick
