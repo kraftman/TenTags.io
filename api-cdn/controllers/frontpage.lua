92d42b22261c730ff36ed781d0972c9e1e62fcb3
Chris Tanner <me@itschr.is>
2016-07-07 18:56:48 +0100

    added method to change post element styling

--- api-cdn/controllers/frontpage.lua
+++ api-cdn/controllers/frontpage.lua
@@ -3,6 +3,28 @@ local api = require 'api.api'
 
 local m = {}
 
+local postTemplates = {
+  default = 'views.st.postelement',
+  minimal = 'views.st.postelement-min'
+}
+
+local function GetPostTemplate(self)
+
+  local postStyle = 'default'
+  if self.session.userID then
+    postStyle = 'default'
+  else
+    postStyle = 'default'
+  end
+  postStyle = 'default'
+
+  if not postTemplates[postStyle] then
+    return postTemplates.default
+  end
+
+  return postTemplates[postStyle]
+end
+
 
 local function FrontPage(self)
   self.pageNum = self.params.page or 1
@@ -10,7 +32,7 @@ local function FrontPage(self)
   local filter = self.req.parsed_url.path:match('/(%w+)$')
 
   self.posts = api:GetUserFrontPage(self.session.userID or 'default',filter,range)
-  -- if empty and logged in then redirect to seen posts
+
   if self.session.userID then
     for _,v in pairs(self.posts) do
       if v.id then
@@ -19,11 +41,14 @@ local function FrontPage(self)
     end
   end
 
+  -- if empty and logged in then redirect to seen posts
   if not self.posts or #self.posts == 0 then
     if filter ~= 'seen' then
       --return { redirect_to = self:url_for("seen") }
     end
   end
+
+  self.GetPostTemplate = GetPostTemplate
 
   return {render = 'frontpage'}
 end
