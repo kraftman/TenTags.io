f546cf0d502f7299223a01ccb7a27aaba7b0345b
Chris Tanner <chris.tanner@sslgb.com>
2017-05-17 16:27:58 +0100

    disable loadnewposts

--- api-cdn/static/js/site.js
+++ api-cdn/static/js/site.js
@@ -16,7 +16,7 @@ $(function() {
   AddFilterSearch();
   GetUserSettings();
   LoadKeybinds();
-  LoadNewPosts();
+  //LoadNewPosts();
   AddToSeenPosts();
   AddFilterHandler();
   LoadUserFilters();
