f19ca39b2bdc8b7985ce0f421c6d7da0fc65c8a1
Chris Tanner <me@itschr.is>
2018-05-29 22:15:21 -0700

    fix tests

--- api-cdn/testing/unit/redis/redisread_spec.lua
+++ api-cdn/testing/unit/redis/redisread_spec.lua
@@ -1,8 +1,18 @@
 
 package.path = package.path.. "./controllers/?.lua;;./lib/?.lua;;";
 
+local base = require 'redis.base'
 local redisread = require 'redis.redisread'
 
+-- local fakeRedis = {}
+
+--   fakeRedis.init_pipeline = function() 
+--     return true
+--   end
+--   fakeRedis.zrevrange = function()
+--     return true
+--   end
+-- end
 
 describe('tests redisread', function()
   it('converts a list to a table', function()
@@ -10,5 +20,7 @@ describe('tests redisread', function()
     local tableOut = redisread:ConvertListToTable(tableIn);
     assert.are.equal(tableOut[1], 'value 1')
   end)
+
+
 
 end)
